99ad5e9e0315e7fc37be3018e5d89e2c32ada458
George-Doge <105048210+George-Doge@users.noreply.github.com>
2022-07-15 11:48:11 +0200

    Add files via upload

--- Graph for functions.py
+++ Graph for functions.py
@@ -5,27 +5,29 @@ from sympy import sympify, SympifyError, plot, Symbol
 
 def manual():
     print("This is a manual for this program. If you have some questions you can read this."
-          "Multiplication is '*' division is '/' and root is '**'.\nProgram can only work with one variable 'x'."
-          "You can save your graphs, all you need to do is to choose a name.\nProgram saves them in .png "
-          "format and into folder where this Python file is.\n")
+          "Multiplication is '*' division is '/' and root is '**'.\nAbsolute power is abs(). Program can only work with"
+          " one variable 'x'. You can save your graphs, all you need to do is to choose a name.\nProgram saves "
+          "them in .png format and into folder where this Python file is.\nNOTE! Right now saving "
+          "is NOT working when you open it as a .py file.")
 
 
 def printing():
-    fun = input("Please enter a function: ")
+    fun = input("\nPlease enter a function: ")
     try:
         fun = sympify(fun)
     except SympifyError:
         print("Invalid input. Check manual for help")
     else:
         picture = plot(fun, (x, x_min, x_max), legend=True)
-    saving(picture)
+        saving(picture)
 
 
 def saving(picture):
-    an = input("Do you want to save your picture? Press 'y' ")
+    an = input("\nDo you want to save your picture? Press 'y' ")
     if an == "y":
         name = input("Choose a name for your picture ")
         picture.save(f"{name}.png")
+        print(f"Picture saved as {name}.png\n")
 
 
 if __name__ == "__main__":
@@ -34,8 +36,7 @@ if __name__ == "__main__":
     if man == "y":
         manual()
     p = 0
-
-    pf = int(input("Write how many graphs do you want to print: "))
+    pf = int(input("\nWrite how many graphs do you want to print: "))
     # Settings, which apply to all printed graphs
     x_min = int(input("Enter minimal value for 'x' axis: "))
     x_max = int(input("Enter maximum value for 'x' axis: "))
@@ -44,4 +45,5 @@ if __name__ == "__main__":
     while p < pf:
         printing()
         p += 1
-    print("Thanks for using this program")
+    print("\nThanks for using this program")
+    input("Press any button to quit ")
